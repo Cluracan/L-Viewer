32977f9f4fd17acf2ad9b122c7f3141fb059dd69
cluracan1 <cluracan1@gmail.com>
2025-12-17 20:54:09 +0000

    chore: remove unused theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Dashboard } from "./components/Dashboard";
-
-const theme = createTheme({
-  colorSchemes: {
-    dark: true,
-  },
-});
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Dashboard />
-      </ThemeProvider>
+      <Dashboard />
     </>
   );
 }
